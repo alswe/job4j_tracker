9e1fd3542fc2616c3cb18f293eb781a486091a0c
alswe <alluginov@hotmail.com>
2020-11-08 18:46:46 -0500

    4. Преобразование List в Map. [#284981]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -8,4 +9,10 @@ public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
     }
+
+    public Map<String, Student> listToMap(List<Student> students) {
+        return students.stream()
+                .collect(Collectors.toMap(Student::getSurname, student -> student));
+    }
+
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -62,6 +63,23 @@ public class SchoolTest {
         expected.add(new Student(20, "Surname2"));
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void listToMap() {
+        School sc = new School();
+        Map<String, Student> rsl = sc.listToMap(students);
+        Map<String, Student> expected = Map.of(
+                "Surname1", new Student(10, "Surname1"),
+                "Surname2", new Student(20, "Surname2"),
+                "Surname3", new Student(30, "Surname3"),
+                "Surname4", new Student(40, "Surname4"),
+                "Surname5", new Student(50, "Surname5"),
+                "Surname6", new Student(60, "Surname6"),
+                "Surname7", new Student(70, "Surname7"),
+                "Surname8", new Student(80, "Surname8"),
+                "Surname9", new Student(90, "Surname9"));
         assertThat(rsl, is(expected));
     }
 }
